bc108d26e0971d78fabcbff826238aef6b7c432c
ll560 <llavia6@gmail.com>
2021-03-19 14:15:20 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let itemName = prompt('Enter the name of the item');
+let basePrice = prompt('What is the base price of the item?');
+let blackFriday = prompt('Is today"Black Friday"?');
+let searchEngine = prompt('Did the purchaser find the product through a search engine?');
+let comparisonShopping = prompt('Did the purchaser visit a comparison-shopping site?')
+
+if (blackFriday = "yes"){
+    
+}
